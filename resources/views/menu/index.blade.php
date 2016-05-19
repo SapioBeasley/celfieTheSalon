4c7aa9c292c205c2cb1f2998f85f447c08edd1bd
Andreas Beasley <andreas.beasley@gmail.com>
2016-05-19 08:05:18 -0700

    images added and some implemented

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -56,7 +56,7 @@
 
 								<div class="col-sm-8">
 									<div class="small-showcase-box">
-										<img src="{{asset('img/desk.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<img src="{{asset('img/brush.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
 										<h4 class="heading">Celfish Lover, <span>up to 2 hours</span></h4>
 										<p class="description">Luscious lashes up to 70% full.</p>
 										<p class="description">Master Artist $175</p>
@@ -67,7 +67,7 @@
 
 								<div class="col-sm-8">
 									<div class="small-showcase-box">
-										<img src="{{asset('img/desk.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<img src="{{asset('img/gloss.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
 										<h4 class="heading">Celfish Cutie, <span>up to 1.5 hours</span></h4>
 										<p class="description">Up to 50% full, naturally enhanced.</p>
 										<p class="description">Master Artist $---</p>
@@ -83,7 +83,7 @@
 
 			<section class="section demo-blocks-section no-margin">
 				<div class="demo-blocks">
-					<p class="demo-blocks-footer align-center">More layouts on the way</p>
+					<p class="demo-blocks-footer align-center">2-3 weeks after initial lash service</p>
 				</div>
 			</section>
 
@@ -94,7 +94,7 @@
 							<div class="row">
 								<div class="col-sm-8">
 									<div class="small-showcase-box">
-										<img src="{{asset('img/desk.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<img src="{{asset('img/hair-dryer.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
 										<h4 class="heading">Bombshell Fill</h4>
 										<p class="description">Master Artist $95</p>
 										<p class="description">Apprentice $75</p>
@@ -104,7 +104,7 @@
 
 								<div class="col-sm-8">
 									<div class="small-showcase-box">
-										<img src="{{asset('img/desk.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<img src="{{asset('img/hair-spray.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
 										<h4 class="heading">Celfish Lover Fill</h4>
 										<p class="description">Master Artist $99</p>
 										<p class="description">Apprentice $75</p>
@@ -114,7 +114,7 @@
 
 								<div class="col-sm-8">
 									<div class="small-showcase-box">
-										<img src="{{asset('img/desk.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<img src="{{asset('img/hair-straightener.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
 										<h4 class="heading">Celfish Cutie</h4>
 										<p class="description">Master Artist $59</p>
 										<p class="description">Apprentice $49</p>
@@ -129,6 +129,88 @@
 		</div>
 
 		<div id="nr-2" class="tab-item">
+			<section class="section section-showcase">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-20 col-lg-offset-2">
+							<div class="row">
+								<div class="col-sm-8">
+									<div class="small-showcase-box">
+										<img src="{{asset('img/mascara.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<h4 class="heading">Fusion (uTip)</h4>
+										<p class="description">$500 +</p>
+										<span class="box-footer">x</span>
+									</div>
+								</div>
+
+								<div class="col-sm-8">
+									<div class="small-showcase-box">
+										<img src="{{asset('img/nail-polish.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<h4 class="heading">Tape</h4>
+										<p class="description">$350 +</p>
+										<span class="box-footer">x</span>
+									</div>
+								</div>
+
+								<div class="col-sm-8">
+									<div class="small-showcase-box">
+										<img src="{{asset('img/perfume-1.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<h4 class="heading">Sewn-in Weft</h4>
+										<p class="description">$80/track</p>
+										<span class="box-footer">x</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			{{-- <section class="section demo-blocks-section no-margin">
+				<div class="demo-blocks">
+					<p class="demo-blocks-footer align-center">More layouts on the way</p>
+				</div>
+			</section> --}}
+
+			<section class="section section-showcase">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-20 col-lg-offset-2">
+							<div class="row">
+								<div class="col-sm-8">
+									<div class="small-showcase-box">
+										<img src="{{asset('img/hand-mirror.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<h4 class="heading">
+										<h4 class="heading">Clip-in's</h4>
+										<p class="description">$200</p>
+									</div>
+								</div>
+
+								<div class="col-sm-8">
+									<div class="small-showcase-box">
+										<img src="{{asset('img/lotion.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<h4 class="heading">I-Tips (microbeads)</h4>
+										<p class="description">$500 +</p>
+										<span class="box-footer">x</span>
+									</div>
+								</div>
+
+								<div class="col-sm-8">
+									<div class="small-showcase-box">
+										<img src="{{asset('img/makeup-1.svg')}}" style="width: 70px;margin-bottom: 20px;"/>
+										<h4 class="heading">Removal</h4>
+										<p class="description">$100</p>
+										<span class="box-footer">x</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+
+		<div id="nr-3" class="tab-item">
 			<section class="section section-showcase">
 				<div class="container-fluid">
 					<div class="row">
@@ -219,7 +301,7 @@
 			</section>
 		</div>
 
-		<div id="nr-3" class="tab-item">
+		<div id="nr-4" class="tab-item">
 			<section class="section section-showcase">
 				<div class="container-fluid">
 					<div class="row">
